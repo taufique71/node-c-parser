93fa0edccbededd4daadb46bde29d76b0192dbad
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-01-04 19:23:22 +0600

    Experiment going on on lexer

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
 var parser = require("../index");
+var fs = require("fs");
+var async = require("async");
 
 describe('helloWorld test', function () {
     it('should pass', function () {
