e7b7d4c699de8526c57e99324bf97d0f9f97fa82
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-01-14 15:34:53 +0600

    Lexer bug fixed
    `Can't find global of undefined patter` bug fixed.

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 var parser = require("./index");
 
 parser.readSourceFile("./test/a.c", function(err, text){
-    console.log(text);
     var x = parser.getTokenStream(text);
     for(var i = 0; i < x.length; i++){
-        console.log(x.lexeme);
+        console.log(x[i].lexeme);
     }
 });
--- test/a.c
+++ test/a.c
@@ -1,4 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
 int main(){
     int x = 10;
+    double y = 3.45;
+    char z[10] = "Hello world";
+    printf("%s", "Hello parser!!!");
     return 0;
 }
