ff73c727fb8a5fc61ccf6649bfb93ee9649bb467
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-03-21 19:56:35 +0600

    lodash added upto relational_expr rules

--- test/main.js
+++ test/main.js
@@ -1,9 +1,9 @@
 var parser = require("../index");
 
-//parser.lexer.cppUnit.clearPreprocessors("./b.c", function(err, codeText){
-    //console.log(codeText);
-    //var tokens = parser.lexer.lexUnit.tokenize(codeText);
-    //console.log(tokens);
-//});
-
-parser.parse();
+parser.lexer.cppUnit.clearPreprocessors("./b.c", function(err, codeText){
+    console.log(codeText);
+    var tokens = parser.lexer.lexUnit.tokenize(codeText);
+    console.log(tokens);
+    var obj = parser.parse(tokens);
+    console.log(obj);
+});
