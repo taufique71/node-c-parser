8b09f430a71fe18be4588dc759f07b1d1af626a4
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-04-11 19:11:24 +0600

    Required file changed in `node-c-parser`

--- extra/module_test/main.js
+++ extra/module_test/main.js
@@ -1,13 +1,10 @@
-var parser = require("../index");
+var parser = require("../../index");
+//console.log(parser);
 
-//parser.lexer.cppUnit.clearPreprocessors("./b.c", function(err, codeText){
+parser.lexer.cppUnit.clearPreprocessors("./b.c", function(err, codeText){
     //console.log(codeText);
-    //var tokens = parser.lexer.lexUnit.tokenize(codeText);
+    var tokens = parser.lexer.lexUnit.tokenize(codeText);
     //console.log(tokens);
-    //var obj = parser.parse(tokens);
-    //console.log(obj);
-//});
-
-var param_type_list = require("../lib/parameter_type_list").parameter_type_list;
-console.log(typeof(param_type_list));
-console.log(typeof(parser));
+    var obj = parser.parse(tokens);
+    console.log(obj);
+});
