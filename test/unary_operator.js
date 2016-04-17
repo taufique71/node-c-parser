360a493cd8e605d6171d8bcb07543c2ddb0cfd22
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-04-17 19:35:03 +0600

    Unit test written for `unary_operator`

--- test/unary_operator.js
+++ test/unary_operator.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var expect = require("chai").expect;
 
 describe('unary_operator', function() {
     it('Should be able to require `unary_operator` as a function', function () {
@@ -7,17 +8,99 @@ describe('unary_operator', function() {
         assert(typeof(unary_operator), "function");
     });
 
+    it("Should recognize '&' as unary operator", function(){
+        var unary_operator = require("../lib/rules").unary_operator;
+        var arrow = { "pointer": 0};
+        var token_stream = [{
+            "lexeme": "&",
+            "row": 5,
+            "col": 3,
+            "tokenClass": "&",
+            "keyword": false,
+            "parent": null,
+            "child": null
+        }];
+        resulting_json = unary_operator(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+    });
+
+    it("Should recognize '*' as unary operator", function(){
+        var unary_operator = require("../lib/rules").unary_operator;
+        var arrow = { "pointer": 0};
+        var token_stream = [{
+            "lexeme": "*",
+            "row": 5,
+            "col": 3,
+            "tokenClass": "*",
+            "keyword": false,
+            "parent": null,
+            "child": null
+        }];
+        resulting_json = unary_operator(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+    });
+
     it("Should recognize '+' as unary operator", function(){
         var unary_operator = require("../lib/rules").unary_operator;
-        var arrow = { "pointer": 1};
+        var arrow = { "pointer": 0};
         var token_stream = [{
             "lexeme": "+",
             "row": 5,
             "col": 3,
             "tokenClass": "+",
-            "keyword": False,
+            "keyword": false,
             "parent": null,
             "child": null
         }];
+        resulting_json = unary_operator(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+    });
+
+    it("Should recognize '-' as unary operator", function(){
+        var unary_operator = require("../lib/rules").unary_operator;
+        var arrow = { "pointer": 0};
+        var token_stream = [{
+            "lexeme": "-",
+            "row": 5,
+            "col": 3,
+            "tokenClass": "-",
+            "keyword": false,
+            "parent": null,
+            "child": null
+        }];
+        resulting_json = unary_operator(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+    });
+
+    it("Should recognize '~' as unary operator", function(){
+        var unary_operator = require("../lib/rules").unary_operator;
+        var arrow = { "pointer": 0};
+        var token_stream = [{
+            "lexeme": "~",
+            "row": 5,
+            "col": 3,
+            "tokenClass": "~",
+            "keyword": false,
+            "parent": null,
+            "child": null
+        }];
+        resulting_json = unary_operator(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+    });
+
+    it("Should recognize '!' as unary operator", function(){
+        var unary_operator = require("../lib/rules").unary_operator;
+        var arrow = { "pointer": 0};
+        var token_stream = [{
+            "lexeme": "!",
+            "row": 5,
+            "col": 3,
+            "tokenClass": "!",
+            "keyword": false,
+            "parent": null,
+            "child": null
+        }];
+        resulting_json = unary_operator(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
     });
 });
