e2e09a79b4975774c42cfd363740a0495325c44e
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-04-27 16:23:22 +0600

    Test cases for unit testing of conditional expression added

--- test/conditional_expr.js
+++ test/conditional_expr.js
@@ -1,9 +1,98 @@
 var assert = require('assert');
+var expect = require("chai").expect;
+var Ajv = require("ajv");
+var ajv = new Ajv();
+var schema = require("../lib/parse-tree-schema");
+var validate = ajv.compile(schema);
 
 describe('conditional_expr', function() {
     it('Should be able to require `conditional_expr` as a function', function () {
         var conditional_expr = require("../lib/rules").conditional_expr;
         assert(conditional_expr);
         assert(typeof(conditional_expr), "function");
+    });
+
+    it("Should recognize '1 < 2 ? 1 : 2' as conditional expression", function(){
+        var conditional_expr = require("../lib/rules").conditional_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: '1',
+            row: 1,
+            col: 1,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: '<',
+            row: 1,
+            col: 3,
+            tokenClass: '<',
+            parent: null,
+            children: null },
+          { lexeme: '2',
+            row: 1,
+            col: 5,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: '?',
+            row: 1,
+            col: 7,
+            tokenClass: '?',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 9,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: ':',
+            row: 1,
+            col: 11,
+            tokenClass: ':',
+            parent: null,
+            children: null },
+          { lexeme: '2',
+            row: 1,
+            col: 13,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = conditional_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("conditional_expr");
+    });
+
+    it("Should recognize '1 || 0' as conditional expression", function(){
+        var conditional_expr = require("../lib/rules").conditional_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: '1',
+            row: 1,
+            col: 1,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: '||',
+            row: 1,
+            col: 3,
+            tokenClass: 'OR_OP',
+            parent: null,
+            children: null },
+          { lexeme: '0',
+            row: 1,
+            col: 6,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = conditional_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("conditional_expr");
     });
 });
