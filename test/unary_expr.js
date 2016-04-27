2c76c28a73d13bd1508683580137a220a1e7def5
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-04-27 15:10:11 +0600

    Test cases for unit test for unary expression added

--- test/unary_expr.js
+++ test/unary_expr.js
@@ -34,7 +34,198 @@ describe('unary_expr', function() {
         expect(resulting_json).to.not.be.null;
         expect(validate(resulting_json)).to.equal(true);
         expect(resulting_json.title).to.equal("unary_expr");
-        expect(resulting_json.children.length).to.equal(1);
-        expect(resulting_json.children[0].title).to.equal("postfix_expr");
+    });
+
+    it("Should recognize '++a' as unary expression", function(){
+        var unary_expr = require("../lib/rules").unary_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+              { lexeme: '++',
+                row: 1,
+                col: 2,
+                tokenClass: 'INC_OP',
+                parent: null,
+                children: null },
+              { lexeme: 'a',
+                row: 1,
+                col: 1,
+                tokenClass: 'IDENTIFIER',
+                parent: null,
+                children: null }
+        ];
+
+        resulting_json = unary_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("unary_expr");
+    });
+
+    it("Should recognize 'a--' as unary expression", function(){
+        var unary_expr = require("../lib/rules").unary_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+              { lexeme: 'a',
+                row: 1,
+                col: 1,
+                tokenClass: 'IDENTIFIER',
+                parent: null,
+                children: null },
+              { lexeme: '--',
+                row: 1,
+                col: 2,
+                tokenClass: 'DEC_OP',
+                parent: null,
+                children: null } 
+        ];
+
+        resulting_json = unary_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("unary_expr");
+    });
+
+    it("Should recognize '--a' as unary expression", function(){
+        var unary_expr = require("../lib/rules").unary_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+              { lexeme: '--',
+                row: 1,
+                col: 2,
+                tokenClass: 'DEC_OP',
+                parent: null,
+                children: null },
+              { lexeme: 'a',
+                row: 1,
+                col: 1,
+                tokenClass: 'IDENTIFIER',
+                parent: null,
+                children: null }
+        ];
+
+        resulting_json = unary_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("unary_expr");
+    });
+
+    it("Should recognize 'sizeof(int)' as unary expression", function(){
+        var unary_expr = require("../lib/rules").unary_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: 'SIZEOF',
+            row: 1,
+            col: 1,
+            tokenClass: 'IDENTIFIER',
+            parent: null,
+            children: null },
+          { lexeme: '(',
+            row: 1,
+            col: 7,
+            tokenClass: '(',
+            parent: null,
+            children: null },
+          { lexeme: 'int',
+            row: 1,
+            col: 9,
+            tokenClass: 'INT',
+            keyword: true,
+            parent: null,
+            children: null },
+          { lexeme: ')',
+            row: 1,
+            col: 13,
+            tokenClass: ')',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = unary_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("unary_expr");
+    });
+
+    it("Should recognize 'sizeof(a++)' as unary expression", function(){
+        var unary_expr = require("../lib/rules").unary_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: 'sizeof',
+            row: 1,
+            col: 1,
+            tokenClass: 'SIZEOF',
+            keyword: true,
+            parent: null,
+            children: null },
+          { lexeme: '(',
+            row: 1,
+            col: 7,
+            tokenClass: '(',
+            parent: null,
+            children: null },
+          { lexeme: 'a',
+            row: 1,
+            col: 8,
+            tokenClass: 'IDENTIFIER',
+            parent: null,
+            children: null },
+          { lexeme: '++',
+            row: 1,
+            col: 9,
+            tokenClass: 'INC_OP',
+            parent: null,
+            children: null },
+          { lexeme: ')',
+            row: 1,
+            col: 11,
+            tokenClass: ')',
+            parent: null,
+            children: null } 
+        ];
+        resulting_json = unary_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("unary_expr");
+    });
+
+    it("Should recognize '+ ( float ) 12' as unary expression", function(){
+        var unary_expr = require("../lib/rules").unary_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: '+',
+            row: 1,
+            col: 1,
+            tokenClass: '+',
+            parent: null,
+            children: null },
+          { lexeme: '(',
+            row: 1,
+            col: 3,
+            tokenClass: '(',
+            parent: null,
+            children: null },
+          { lexeme: 'float',
+            row: 1,
+            col: 5,
+            tokenClass: 'FLOAT',
+            keyword: true,
+            parent: null,
+            children: null },
+          { lexeme: ')',
+            row: 1,
+            col: 11,
+            tokenClass: ')',
+            parent: null,
+            children: null },
+          { lexeme: '12',
+            row: 1,
+            col: 13,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null } 
+        ];
+        resulting_json = unary_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("unary_expr");
     });
 });
