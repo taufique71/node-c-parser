a85139e19edd53d98867e5745d66df9d091e1a85
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-04-27 16:45:46 +0600

    Test cases for unit testing of assignment expressions done

--- test/assignment_expr.js
+++ test/assignment_expr.js
@@ -1,9 +1,86 @@
 var assert = require('assert');
+var expect = require("chai").expect;
+var Ajv = require("ajv");
+var ajv = new Ajv();
+var schema = require("../lib/parse-tree-schema");
+var validate = ajv.compile(schema);
 
 describe('assignment_expr', function() {
     it('Should be able to require `assignment_expr` as a function', function () {
         var assignment_expr = require("../lib/rules").assignment_expr;
         assert(assignment_expr);
         assert(typeof(assignment_expr), "function");
+    });
+
+    it("Should recognize 'a = a + 1' as assignment expression", function(){
+        var assignment_expr = require("../lib/rules").assignment_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: 'a',
+            row: 1,
+            col: 1,
+            tokenClass: 'IDENTIFIER',
+            parent: null,
+            children: null },
+          { lexeme: '=',
+            row: 1,
+            col: 3,
+            tokenClass: '=',
+            parent: null,
+            children: null },
+          { lexeme: 'a',
+            row: 1,
+            col: 5,
+            tokenClass: 'IDENTIFIER',
+            parent: null,
+            children: null },
+          { lexeme: '+',
+            row: 1,
+            col: 7,
+            tokenClass: '+',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 9,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = assignment_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("assignment_expr");
+    });
+
+    it("Should recognize 'a += 1' as assignment expression", function(){
+        var assignment_expr = require("../lib/rules").assignment_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: 'a',
+            row: 1,
+            col: 1,
+            tokenClass: 'IDENTIFIER',
+            parent: null,
+            children: null },
+          { lexeme: '+=',
+            row: 1,
+            col: 3,
+            tokenClass: 'ADD_ASSIGN',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 6,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = assignment_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("assignment_expr");
     });
 });
