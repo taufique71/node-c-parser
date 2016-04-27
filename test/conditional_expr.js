f46d1c27165358dca37a86c5753b7b17d1192a6e
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-04-27 19:17:49 +0600

    Another test cases added for unit testing of conditional expressions

--- test/conditional_expr.js
+++ test/conditional_expr.js
@@ -66,6 +66,150 @@ describe('conditional_expr', function() {
         expect(resulting_json.title).to.equal("conditional_expr");
     });
 
+    it("Should recognize '(1 < 2) ? 1 : 2' as conditional expression", function(){
+        var conditional_expr = require("../lib/rules").conditional_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: '(',
+            row: 1,
+            col: 1,
+            tokenClass: '(',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 2,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: '<',
+            row: 1,
+            col: 4,
+            tokenClass: '<',
+            parent: null,
+            children: null },
+          { lexeme: '2',
+            row: 1,
+            col: 6,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: ')',
+            row: 1,
+            col: 7,
+            tokenClass: ')',
+            parent: null,
+            children: null },
+          { lexeme: '?',
+            row: 1,
+            col: 9,
+            tokenClass: '?',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 11,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: ':',
+            row: 1,
+            col: 13,
+            tokenClass: ':',
+            parent: null,
+            children: null },
+          { lexeme: '2',
+            row: 1,
+            col: 15,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = conditional_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("conditional_expr");
+    });
+
+    it("Should recognize '((1 < 2) ? 1 : 2)' as conditional expression", function(){
+        var conditional_expr = require("../lib/rules").conditional_expr;
+        var arrow = { "pointer": 0};
+        var token_stream = [ 
+          { lexeme: '(',
+            row: 1,
+            col: 1,
+            tokenClass: '(',
+            parent: null,
+            children: null },
+          { lexeme: '(',
+            row: 1,
+            col: 2,
+            tokenClass: '(',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 3,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: '<',
+            row: 1,
+            col: 5,
+            tokenClass: '<',
+            parent: null,
+            children: null },
+          { lexeme: '2',
+            row: 1,
+            col: 7,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: ')',
+            row: 1,
+            col: 8,
+            tokenClass: ')',
+            parent: null,
+            children: null },
+          { lexeme: '?',
+            row: 1,
+            col: 10,
+            tokenClass: '?',
+            parent: null,
+            children: null },
+          { lexeme: '1',
+            row: 1,
+            col: 12,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: ':',
+            row: 1,
+            col: 14,
+            tokenClass: ':',
+            parent: null,
+            children: null },
+          { lexeme: '2',
+            row: 1,
+            col: 16,
+            tokenClass: 'CONSTANT',
+            parent: null,
+            children: null },
+          { lexeme: ')',
+            row: 1,
+            col: 17,
+            tokenClass: ')',
+            parent: null,
+            children: null } 
+        ];
+
+        resulting_json = conditional_expr(token_stream, arrow);
+        expect(resulting_json).to.not.be.null;
+        expect(validate(resulting_json)).to.equal(true);
+        expect(resulting_json.title).to.equal("conditional_expr");
+    });
+
     it("Should recognize '1 || 0' as conditional expression", function(){
         var conditional_expr = require("../lib/rules").conditional_expr;
         var arrow = { "pointer": 0};
