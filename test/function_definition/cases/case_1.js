7492a3cdd232a165d210fdf0e94795ec21939bfc
Md. Taufique Hussain <taufique@muktosoft.com>
2016-11-01 20:00:58 +0600

    Changed function definition unit testing structure

---
+++ test/function_definition/cases/case_1.js
@@ -0,0 +1,151 @@
+[
+  {
+    "lexeme": "int",
+    "row": 1,
+    "col": 1,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "main",
+    "row": 1,
+    "col": 5,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "(",
+    "row": 1,
+    "col": 9,
+    "tokenClass": "(",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ")",
+    "row": 1,
+    "col": 10,
+    "tokenClass": ")",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "{",
+    "row": 1,
+    "col": 11,
+    "tokenClass": "{",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "int",
+    "row": 2,
+    "col": 5,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "a",
+    "row": 2,
+    "col": 9,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 2,
+    "col": 10,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "int",
+    "row": 3,
+    "col": 5,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "b",
+    "row": 3,
+    "col": 9,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 3,
+    "col": 10,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "int",
+    "row": 4,
+    "col": 5,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "c",
+    "row": 4,
+    "col": 9,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 4,
+    "col": 10,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "return",
+    "row": 5,
+    "col": 5,
+    "tokenClass": "RETURN",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "0",
+    "row": 5,
+    "col": 12,
+    "tokenClass": "CONSTANT",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 5,
+    "col": 13,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "}",
+    "row": 6,
+    "col": 1,
+    "tokenClass": "}",
+    "parent": null,
+    "children": null
+  }
+]
--- test/function_definition/function_definition.js
+++ test/function_definition/function_definition.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('function_definition', function() {
     it('Should be able to require `function_definition` as a function', function () {
-        var function_definition = require("../lib/rules").function_definition;
+        var function_definition = require("../../lib/rules").function_definition;
         assert(function_definition);
         assert(typeof(function_definition), "function");
     });
