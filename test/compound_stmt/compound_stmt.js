59778553092f0638e15b05cf3b4143d5d260d8e8
Md. Taufique Hussain <taufique@muktosoft.com>
2016-11-02 18:34:19 +0600

    Fixes some unit testing cases for compound_stmt and function_definition

--- test/compound_stmt/compound_stmt.js
+++ test/compound_stmt/compound_stmt.js
@@ -1,6 +1,10 @@
 var assert = require("assert");
 var expect = require("chai").expect;
 var jsonfile = require("jsonfile");
+var Ajv = require("ajv");
+var ajv = new Ajv();
+var schema = require("../../lib/parse-tree-schema");
+var validate = ajv.compile(schema);
 
 describe('compound_stmt', function() {
     it('Should be able to require `compound_stmt` as a function', function () {
@@ -9,13 +13,13 @@ describe('compound_stmt', function() {
         assert(typeof(compound_stmt), "function");
     });
 
-    it("case_2 should be realized as compound statement", function(){
+    it("case_2 should be realized as compound statement", function(done){
         var compound_stmt = require("../../lib/rules").compound_stmt;
-        var file = __dirname + "/compound_stmt/cases/case_2.js"
-        jsonfile.readFile(file, function(err, token_strem){
+        var file = __dirname + "/cases/case_2.js"
+        jsonfile.readFile(file, function(err, token_stream){
             if(err) done(err);
             else{
-                var arrow = { "pointer": 0};
+                var arrow = { "pointer": 0 };
                 resulting_json = compound_stmt(token_stream, arrow);
                 expect(resulting_json).to.not.be.null;
                 expect(validate(resulting_json)).to.equal(true);
@@ -24,13 +28,13 @@ describe('compound_stmt', function() {
         });
     });
 
-    it("case_1 should be realized as compound statement", function(){
+    it("case_1 should be realized as compound statement", function(done){
         var compound_stmt = require("../../lib/rules").compound_stmt;
-        var file = __dirname + "/compound_stmt/cases/case_1.js"
-        jsonfile.readFile(file, function(err, token_strem){
+        var file = __dirname + "/cases/case_1.js"
+        jsonfile.readFile(file, function(err, token_stream){
             if(err) done(err);
             else{
-                var arrow = { "pointer": 0};
+                var arrow = { "pointer": 0 };
                 resulting_json = compound_stmt(token_stream, arrow);
                 expect(resulting_json).to.not.be.null;
                 expect(validate(resulting_json)).to.equal(true);
--- test/function_definition/function_definition.js
+++ test/function_definition/function_definition.js
@@ -1,9 +1,27 @@
-var assert = require('assert');
+var assert = require("assert");
+var expect = require("chai").expect;
+var jsonfile = require("jsonfile");
 
 describe('function_definition', function() {
     it('Should be able to require `function_definition` as a function', function () {
         var function_definition = require("../../lib/rules").function_definition;
         assert(function_definition);
         assert(typeof(function_definition), "function");
+    });
+    it("case_1 should be realized as function_definition", function(done){
+        var function_definition = require("../../lib/rules").function_definition;
+        var file = __dirname + "/cases/case_1.js"
+        jsonfile.readFile(file, function(err, token_stream){
+            if(err){
+                done(err);
+            }
+            else{
+                var arrow = { "pointer": 0 };
+                resulting_json = function_definition(token_stream, arrow);
+                expect(resulting_json).to.not.be.null;
+                expect(validate(resulting_json)).to.equal(true);
+                done();
+            }
+        });
     });
 });
