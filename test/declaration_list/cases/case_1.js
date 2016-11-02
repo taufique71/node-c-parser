0a452725d3f89b2821bdbcbe5ae244f36c2cd2b8
Md. Taufique Hussain <taufique@muktosoft.com>
2016-11-02 19:18:55 +0600

    Adds test case for declaration_list

---
+++ test/declaration_list/cases/case_1.js
@@ -0,0 +1,77 @@
+[
+  {
+    "lexeme": "int",
+    "row": 1,
+    "col": 1,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "a",
+    "row": 1,
+    "col": 5,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 1,
+    "col": 6,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "int",
+    "row": 2,
+    "col": 1,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "b",
+    "row": 2,
+    "col": 5,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 2,
+    "col": 6,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "int",
+    "row": 3,
+    "col": 1,
+    "tokenClass": "INT",
+    "keyword": true,
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": "c",
+    "row": 3,
+    "col": 5,
+    "tokenClass": "IDENTIFIER",
+    "parent": null,
+    "children": null
+  },
+  {
+    "lexeme": ";",
+    "row": 3,
+    "col": 6,
+    "tokenClass": ";",
+    "parent": null,
+    "children": null
+  }
+]
---
+++ test/declaration_list/declaration_list.js
@@ -0,0 +1,31 @@
+var assert = require("assert");
+var expect = require("chai").expect;
+var jsonfile = require("jsonfile");
+var Ajv = require("ajv");
+var ajv = new Ajv();
+var schema = require("../../lib/parse-tree-schema");
+var validate = ajv.compile(schema);
+
+describe('declaration_list', function() {
+    it('Should be able to require `declaration_list` as a function', function () {
+        var declaration_list = require("../../lib/rules").declaration_list;
+        assert(declaration_list);
+        assert(typeof(declaration_list), "function");
+    });
+
+    it("case_1 should be realized as declaration_list", function(done){
+        var declaration_list = require("../../lib/rules").declaration_list;
+        var file = __dirname + "/cases/case_1.js"
+        jsonfile.readFile(file, function(err, token_stream){
+            if(err) done(err);
+            else{
+                var arrow = { "pointer": 0 };
+                resulting_json = declaration_list(token_stream, arrow);
+                console.log(resulting_json);
+                expect(resulting_json).to.not.be.null;
+                expect(validate(resulting_json)).to.equal(true);
+                done();
+            }
+        });
+    });
+});
